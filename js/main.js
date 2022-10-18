cbe6d7ce5ce718c79e29f4d914f7aef6c53606f5
Michael <michaelgandolfi7@gmail.com>
2022-10-18 08:22:13 -0500

    added pokemon objects

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
+const poke1Obj = {}
+const poke2Obj = {}
+
 function getFetch(){
   const poke1 = document.querySelector('#poke1').value
   const poke2 = document.querySelector('#poke2').value
@@ -12,14 +15,14 @@ function getFetch(){
       .then(data => {
 
         console.log(data)
-        document.querySelector("#poke1Name").innerText = data.name.toUpperCase()
-        document.querySelector("#poke1Img").src = data.sprites.front_shiny
-        document.querySelector("#hp1").innerText = data.stats[0].base_stat
-        document.querySelector("#attack1").innerText = data.stats[1].base_stat
-        document.querySelector("#defense1").innerText = data.stats[2].base_stat
-        document.querySelector("#specialAttack1").innerText = data.stats[3].base_stat
-        document.querySelector("#specialDefense1").innerText = data.stats[4].base_stat
-        document.querySelector("#speed1").innerText = data.stats[5].base_stat
+        poke1Obj.name = document.querySelector("#poke1Name").innerText = data.name.toUpperCase()
+        poke1Obj.sprite = document.querySelector("#poke1Img").src = data.sprites.front_shiny
+        poke1Obj.hp = document.querySelector("#hp1").innerText = data.stats[0].base_stat
+        poke1Obj.attack = document.querySelector("#attack1").innerText = data.stats[1].base_stat
+        poke1Obj.defense = document.querySelector("#defense1").innerText = data.stats[2].base_stat
+        poke1Obj.specialAttack = document.querySelector("#specialAttack1").innerText = data.stats[3].base_stat
+        poke1Obj.specialDefense = document.querySelector("#specialDefense1").innerText = data.stats[4].base_stat
+        poke1Obj.speed = document.querySelector("#speed1").innerText = data.stats[5].base_stat
 
 
       })
@@ -32,13 +35,13 @@ function getFetch(){
         .then(data => {
             console.log(data)
 
-            document.querySelector("#poke2Name").innerText = data.name.toUpperCase()
-            document.querySelector("#poke2Img").src = data.sprites.front_shiny
-            document.querySelector("#hp2").innerText = data.stats[0].base_stat
-            document.querySelector("#attack2").innerText = data.stats[1].base_stat
-            document.querySelector("#defense2").innerText = data.stats[2].base_stat
-            document.querySelector("#specialAttack2").innerText = data.stats[3].base_stat
-            document.querySelector("#specialDefense2").innerText = data.stats[4].base_stat
-            document.querySelector("#speed2").innerText = data.stats[5].base_stat
+            poke2Obj.name = document.querySelector("#poke2Name").innerText = data.name.toUpperCase()
+            poke2Obj.sprite = document.querySelector("#poke2Img").src = data.sprites.front_shiny
+            poke2Obj.hp = document.querySelector("#hp2").innerText = data.stats[0].base_stat
+            poke2Obj.attack = document.querySelector("#attack2").innerText = data.stats[1].base_stat
+            poke2Obj.defense = document.querySelector("#defense2").innerText = data.stats[2].base_stat
+            poke2Obj.specialAttack = document.querySelector("#specialAttack2").innerText = data.stats[3].base_stat
+            poke2Obj.specialDefense = document.querySelector("#specialDefense2").innerText = data.stats[4].base_stat
+            poke2Obj.speed = document.querySelector("#speed2").innerText = data.stats[5].base_stat
         })     
 }
